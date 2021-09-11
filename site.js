73becc76c1e8f3c7d42cc77d856d1c145a6c3f54
Will Boyd <will@codersblock.com>
2021-09-11 19:53:26 -0400

    Refactored out the class stuff, added SVG buttons

--- site.js
+++ site.js
@@ -1,139 +1,6 @@
-// constants
-const BOARD_COLUMNS_COUNT = 15;
-const BOARD_ROWS_COUNT = 15;
-
-class Board {
-  constructor(columns, rows, el) {
-    this.columns = columns;
-    this.rows = rows;
-    this.el = el;
-    this.isIterating = false;
-
-    // init board state
-    this.board = this.createEmptyBoard();
-    
-    // init board element
-    this.el.textContent = '';
-    this.board.forEach(column => {
-      const columnEl = document.createElement('div');
-      column.forEach(cell => {
-        const cellEl = document.createElement('div');
-        columnEl.append(cellEl);
-      });
-      this.el.append(columnEl);
-    });    
-  }
-
-  createEmptyBoard() {
-    return (
-      Array.from({ length: this.columns }, () =>
-        Array.from({ length: this.rows }, () =>
-          false
-        )
-      )
-    );
-  }
-
-  createLife(x, y) {
-    this.setLife(x, y, true);
-  }
-  
-  endLife(x, y) {
-    this.setLife(x, y, false);
-  }
-  
-  setLife(x, y, isAlive) {
-    if (this.board[x][y] === isAlive) {
-      // nothing to do
-      return;
-    }
-
-    // update cell state
-    this.board[x][y] = isAlive;
-
-    // update cell element
-    const cellEl = this.el.children[x].children[y];
-    cellEl.toggleAttribute('data-is-alive', isAlive)
-  }
-  
-  setLifeFromElement(cellEl) {
-    if (cellEl.parentElement.parentElement !== this.el) {
-      // this is not a cell element
-      return;
-    }
-    
-    const columnEl = cellEl.parentElement;
-    const x = [...this.el.children].indexOf(columnEl);
-    const y = [...columnEl.children].indexOf(cellEl);
-    this.createLife(x, y);
-  }
-
-  countNeighbors(x, y) {
-    // array of coordinate offsets to all 8 (potential) neighbors
-    const offsets = [
-      [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
-    ];
-    
-    return offsets
-      .map(([xOffset, yOffset]) => [x + xOffset, y + yOffset]) // change to absolute positions
-      .filter(([xPos, yPos]) => (
-        xPos >= 0 && xPos < this.columns && // check if within x bounds
-        yPos >= 0 && yPos < this.rows &&    // check if within y bounds
-        this.board[xPos][yPos]              // check if is alive
-      ))
-      .length; // return count of neighbors
-  }
-
-  iterate() {
-    // will hold the next state of the board
-    const nextBoard = this.createEmptyBoard();
-    
-    // first pass, determine next state of all life on current board
-    for (let x = 0; x < this.columns; x++) {
-      for (let y = 0; y < this.rows; y++) {
-        const isAlive = this.board[x][y];
-        const neighbors = this.countNeighbors(x, y);
-        if (isAlive && (neighbors === 2 || neighbors === 3)) {
-          // life continues
-          nextBoard[x][y] = true;
-        } else if (!isAlive && neighbors === 3) {
-          // life is created
-          nextBoard[x][y] = true;
-        }
-      }
-    }
-
-    // second pass, copy state of all life from next to current
-    for (let x = 0; x < this.columns; x++) {
-      for (let y = 0; y < this.rows; y++) {
-        this.setLife(x, y, nextBoard[x][y]);
-      }
-    }
-  }
-  
-  async startIterating() {
-    if (this.isIterating) {
-      // already iterating
-      return;
-    }
-
-    this.isIterating = true;
-    while (true) {
-      this.iterate();
-
-      // sleep
-      await new Promise(resolve => setTimeout(resolve, 200));
-
-      if (!this.isIterating) {
-        break;
-      }
-    }
-  }
-  
-  stopIterating() {
-    this.isIterating = false;
-  }
-}
+// config
+const BOARD_COLUMNS_COUNT = 32;
+const BOARD_ROWS_COUNT = 32;
 
 // elements
 const bodyEl = document.querySelector('body');
@@ -142,29 +9,158 @@ const startEl = document.getElementById('start');
 const stopEl = document.getElementById('stop');
 
 // globals
-const board = new Board(BOARD_COLUMNS_COUNT, BOARD_ROWS_COUNT, boardEl);
+let board = [[]];
 let isDrawing = false;
+let isIterating = false;
+
+function reset() {
+  // init board state
+  board = createEmptyBoard();
+
+  // init board element
+  boardEl.textContent = '';
+  board.forEach(column => {
+    const columnEl = document.createElement('div');
+    column.forEach(cell => {
+      const cellEl = document.createElement('div');
+      columnEl.append(cellEl);
+    });
+    boardEl.append(columnEl);
+  });
+}
+
+function createEmptyBoard() {
+  return (
+    Array.from({ length: BOARD_COLUMNS_COUNT }, () =>
+      Array.from({ length: BOARD_ROWS_COUNT }, () =>
+        false
+      )
+    )
+  );
+}
+
+function setLife(x, y, isAlive) {
+  if (board[x][y] === isAlive) {
+    // nothing to do
+    return;
+  }
+
+  // update cell state
+  board[x][y] = isAlive;
+
+  // update cell element
+  const cellEl = boardEl.children[x].children[y];
+  cellEl.toggleAttribute('data-is-alive', isAlive)
+}
+
+function setLifeFromElement(cellEl, isAlive) {
+  if (cellEl.parentElement.parentElement !== boardEl) {
+    // this is not a cell element
+    return;
+  }
+  
+  const columnEl = cellEl.parentElement;
+  const x = [...boardEl.children].indexOf(columnEl);
+  const y = [...columnEl.children].indexOf(cellEl);
+  setLife(x, y, isAlive);
+}
+
+function countNeighbors(x, y) {
+  // array of coordinate offsets to all 8 (potential) neighbors
+  const offsets = [
+    [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]
+  ];
+
+  return offsets
+    .map(([xOffset, yOffset]) => [x + xOffset, y + yOffset]) // change to absolute positions
+    .filter(([xPos, yPos]) => (
+      xPos >= 0 && xPos < BOARD_COLUMNS_COUNT && // check if within x bounds
+      yPos >= 0 && yPos < BOARD_ROWS_COUNT &&    // check if within y bounds
+      board[xPos][yPos]                          // check if is alive
+    ))
+    .length; // return count of neighbors
+}
+
+function iterate() {
+  // will hold the next state of the board
+  const nextBoard = createEmptyBoard();
+  
+  // first pass, determine next state of all life on current board
+  for (let x = 0; x < BOARD_COLUMNS_COUNT; x++) {
+    for (let y = 0; y < BOARD_ROWS_COUNT; y++) {
+      const isAlive = board[x][y];
+      const neighbors = countNeighbors(x, y);
+      if (isAlive && (neighbors === 2 || neighbors === 3)) {
+        // life continues
+        nextBoard[x][y] = true;
+      } else if (!isAlive && neighbors === 3) {
+        // life is created
+        nextBoard[x][y] = true;
+      }
+    }
+  }
+
+  // second pass, copy state of all life from next to current
+  for (let x = 0; x < BOARD_COLUMNS_COUNT; x++) {
+    for (let y = 0; y < BOARD_ROWS_COUNT; y++) {
+      setLife(x, y, nextBoard[x][y]);
+    }
+  }
+}
+
+async function startIterating() {
+  if (isIterating) {
+    // already iterating
+    return;
+  }
+
+  isIterating = true;
+  startEl.disabled = true;
+  stopEl.disabled = false;
+
+  while (true) {
+    iterate();
+
+    // sleep
+    await new Promise(resolve => setTimeout(resolve, 200));
+
+    if (!isIterating) {
+      break;
+    }
+  }
+}
+
+function stopIterating() {
+  if (!isIterating) {
+    // already not iterating
+    return;
+  }
+
+  isIterating = false;
+  startEl.disabled = false;
+  stopEl.disabled = true;
+}
 
 // start iterating
 startEl.addEventListener('click', () => {
-  board.startIterating();
+  startIterating();
 });
 
 // stop iterating
 stopEl.addEventListener('click', () => {
-  board.stopIterating();
+  stopIterating();
 });
 
 // create life in this cell and start drawing
 boardEl.addEventListener('mousedown', (event) => {
-  board.setLifeFromElement(event.target);
+  setLifeFromElement(event.target, true);
   isDrawing = true;
 });
 
 // create life in cells while drawing
 boardEl.addEventListener('mouseover', (event) => {
   if (isDrawing) {
-    board.setLifeFromElement(event.target);
+    setLifeFromElement(event.target, true);
   }
 });
 
@@ -182,3 +178,5 @@ bodyEl.addEventListener('mouseleave', () => {
 boardEl.addEventListener('dragstart', (event) => {
   event.preventDefault();
 });
+
+reset();
